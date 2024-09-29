bf730d5ef02aa82806d29bf95522aaa9e448d828
Balázs Berecz <bereczbalazs95@gmail.com>
2024-09-29 20:04:32 +0200

    window elements and binding added

--- BertinaAccountingTool/BusinessLogic/ViewModel/CSVInvoiceViewModel.cs
+++ BertinaAccountingTool/BusinessLogic/ViewModel/CSVInvoiceViewModel.cs
@@ -1,7 +1,30 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace BertinaAccountingTool.BusinessLogic.ViewModel;
 
 public partial class CSVInvoiceViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private string sourceFilePath = string.Empty;
+
+    [ObservableProperty]
+    private string rootFolder = string.Empty;
+
+    [RelayCommand]
+    public void SourceBrowse()
+    {
+
+    }
+
+    [RelayCommand]
+    public void RootFolderBrowse()
+    {
+
+    }
+
+    [RelayCommand]
+    public void CreateCSV()
+    {
+    }
 }
