9070a01990671041064792785f16d75f8764f4c8
Berecz, Balazs (ext) <balazs.berecz.ext@siemens-energy.com>
2025-02-13 22:04:13 +0100

    delay added before OK message

--- BertinaAccountingTool/Model/CIBBankPOM/HeaderPage.cs
+++ BertinaAccountingTool/Model/CIBBankPOM/HeaderPage.cs
@@ -3,54 +3,55 @@ using System.Drawing;
 
 namespace BertinaAccountingTool.Model.CIBBankPOM
 {
-    internal class HeaderPage : BasePage
-    {
-        private const string XpathToFindCompanyFromTheList = "/html/body/div[2]/div/div/div[4]/ul/li[1]/ul/li[.//*[contains(text(), '{0}')]]";
+	internal class HeaderPage : BasePage
+	{
+		private const string XpathToFindCompanyFromTheList = "/html/body/div[2]/div/div/div[4]/ul/li[1]/ul/li[.//*[contains(text(), '{0}')]]";
 
-        private IWebElement CurrentCompany => driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[4]/ul/li[1]"));
+		private IWebElement CurrentCompany => driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[4]/ul/li[1]"));
 
-        public HeaderPage(IWebDriver driver) : base(driver)
-        {
-        }
+		public HeaderPage(IWebDriver driver) : base(driver)
+		{
+		}
 
-        internal void OpenCompanyDropDown()
-        {
-            if (CurrentCompany.GetDomAttribute("data-is-click") == "true")
-            {
-                return;
-            }
+		internal void OpenCompanyDropDown()
+		{
+			if (CurrentCompany.GetDomAttribute("data-is-click") == "true")
+			{
+				return;
+			}
 
-            CurrentCompany.Click();
-        }
+			CurrentCompany.Click();
+		}
 
-        internal void SelectCompany(string name)
-        {
-            if (driver.FindElement(By.XPath(string.Format(XpathToFindCompanyFromTheList, name.Substring(0,name.IndexOf(' ')).ToUpper()))) is IWebElement nextCompany)
-            {
-                nextCompany.Click();
-            }
-            else
-            {
-                throw new Exception($"{name} company not found");
-            }
-        }
+		internal void SelectCompany(string name)
+		{
+			if (driver.FindElement(By.XPath(string.Format(XpathToFindCompanyFromTheList, name.Substring(0, name.IndexOf(' ')).ToUpper()))) is IWebElement nextCompany)
+			{
+				nextCompany.Click();
+			}
+			else
+			{
+				throw new Exception($"{name} company not found");
+			}
+		}
 
-        internal void TryPressOK()
-        {
-            try
-            {
-                var prevPos = driver.FindElement(By.XPath("//button[text()='Rendben']")).Location;
-                Thread.Sleep(100);
-                while (driver.FindElement(By.XPath("//button[text()='Rendben']")).Location !=prevPos)
-                {
-                    prevPos = driver.FindElement(By.XPath("//button[text()='Rendben']")).Location;
-                    Thread.Sleep(100);
-                }
-                driver.FindElement(By.XPath("//button[text()='Rendben']")).Click();
-            }
-            catch (Exception)
-            {
-            }
-        }
-    }
+		internal void TryPressOK()
+		{
+			try
+			{
+				Thread.Sleep(1000);
+				var prevPos = driver.FindElement(By.XPath("//button[text()='Rendben']")).Location;
+				Thread.Sleep(100);
+				while (driver.FindElement(By.XPath("//button[text()='Rendben']")).Location != prevPos)
+				{
+					prevPos = driver.FindElement(By.XPath("//button[text()='Rendben']")).Location;
+					Thread.Sleep(100);
+				}
+				driver.FindElement(By.XPath("//button[text()='Rendben']")).Click();
+			}
+			catch (Exception)
+			{
+			}
+		}
+	}
 }
