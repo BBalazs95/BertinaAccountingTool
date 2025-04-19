6f91d7f044ba715b8b73cd85a59f523e6646ef50
Balázs Berecz <bereczbalazs95@gmail.com>
2025-04-19 18:47:32 +0200

    browse added

--- BertinaAccountingTool/BusinessLogic/ViewModel/TEYAViewModel.cs
+++ BertinaAccountingTool/BusinessLogic/ViewModel/TEYAViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
 using System.Collections.ObjectModel;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace BertinaAccountingTool.BusinessLogic.ViewModel;
@@ -17,18 +19,17 @@ internal partial class TEYAViewModel : ObservableObject
     private string? transactionsOutput;
 
     [RelayCommand]
-    private void BuyerInvoivesBrowse()
+    public void SourceBrowse(object textBox)
     {
-    }
+        OpenFileDialog openFileDialog = new();
+        openFileDialog.Filter = "Excel Files|*.xls;*.xlsx";
+        openFileDialog.Title = "Válasz egy excel fájlt";
+        openFileDialog.Multiselect = false;
 
-    [RelayCommand]
-    private void TransactionsBrowse()
-    {
-    }
-
-    [RelayCommand]
-    private void TransactionsOutputBrowse()
-    {
+        if (openFileDialog.ShowDialog() == true && textBox is TextBox box)
+        {
+            box.Text = openFileDialog.FileName;
+        }
     }
 
     [RelayCommand]
